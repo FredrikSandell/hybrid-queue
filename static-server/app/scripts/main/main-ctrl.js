e9e1f1a0b87996e6b470c0c783939c867ac93ce0
Fredrik Sandell <sandell.fredrik@gmail.com>
2015-03-24 19:52:04 +0100

    Added a lot of test code for the stomp client in the angular app

--- static-server/app/scripts/main/main-ctrl.js
+++ static-server/app/scripts/main/main-ctrl.js
@@ -1,66 +1,7 @@
 'use strict';
 
-
-function makeStompClient(IdProvider, $q) {
-    var self = {};
-
-    var ws = new SockJS('http://' + window.location.hostname + ':15674/stomp');
-    var stompClient = Stomp.over(ws);
-    // SockJS does not support heart-beat: disable heart-beats
-    stompClient.heartbeat.outgoing = 0;
-    stompClient.heartbeat.incoming = 0;
-
-    var deferred = $q.defer();
-
-    function makeWrappedClient(stompClient) {
-        var that = {};
-        that.sendQ = function(id, data) {
-            stompClient.send('/queue/'+id, {"content-type":"application/json"}, JSON.stringify(data));
-        };
-        that.sendT = function(id, data) {
-            stompClient.send('/topic/'+id, {"content-type":"application/json"}, JSON.stringify(data));
-        };
-        return that;
-    };
-
-    var listeners = [];
-    self.registerListener = function(id, func) {
-        listeners[id] = func;
-    };
-    self.removeListener = function(id) {
-
-    }
-
-    var notifyListeners = function (data) {
-        angular.forEach(listeners, function(listener) {
-            listener(data);
-        });
-    };
-
-    IdProvider.getId().then(function success(data){
-        var on_connect = function(x) {
-            stompClient.subscribe("/topic/"+data.id, function(d) {
-                console.log(d.body);
-                notifyListeners(JSON.parse(d.body));
-            });
-            deferred.resolve(makeWrappedClient(stompClient));
-        };
-        var on_error =  function() {
-            console.log('error');
-            throw "Failed to connect to message broker";
-        };
-        stompClient.connect('guest', 'guest', on_connect, on_error, '/');
-    }, function error(reason){
-
-    });
-    self.getClient = function(){
-        return deferred.promise;
-    };
-    return self;
-};
-
 angular.module('famousAngularStarter')
-    .controller('MainCtrl', ['$scope','$famous','StompService', function ($scope, $famous, StompService) {
+    .controller('MainCtrl', ['$scope','$famous','StompClientFactory', function ($scope, $famous, StompClientFactory) {
         var Transitionable = $famous['famous/transitions/Transitionable'];
         var Timer = $famous['famous/utilities/Timer'];
 
@@ -75,15 +16,40 @@ angular.module('famousAngularStarter')
             $scope.rotateY.set($scope.rotateY.get() + adjustedSpeed);
         }, 1);
 
-    }])
-    .service('IdProvider', ['$q', function($q) {
-        var self = {};
-        self.getId = function() {
-            var deferred = $q.defer();
-            deferred.resolve("client1");
-            return deferred.promise()
-        };
-        return self;
+        StompClientFactory.then(function success(client){
+            console.log('connected to rabbitmq');
+
+            //using it wih permanent queues
+            client.listenTo('/amq/queue/a-client', function(data) {
+                console.log('got this on a-client queue:');
+                console.log(data.body);
+            });
+            var send = client.makeSender('/amq/queue/a-client');
+            send({
+                msg: '123',
+                status: true
+            });
+
+            //Using it with temporary queues
+            client.listenTo('/queue/temporary-client', function(msg) {
+                console.log('got this on tqueue queue:');
+                console.log(msg.body);
+                client.send(msg.headers['reply-to'], {
+                    msg: "got the message on the temporary queue"
+                });
+            });
+
+            var temporarySender = client.makeTempSender('/queue/temporary-client',function(d){
+                console.log("Got this back on the tmp queue"+d);
+                console.log(d);
+            });
+            temporarySender({
+                msg: "temporaryMsg"
+            });
+        }, function error() {
+            console.log('failed to connect to rabbitmq');
+        });
+
     }])
     .service('Guid',[function() {
         return {
@@ -109,33 +75,38 @@ angular.module('famousAngularStarter')
 
         var deferred = $q.defer();
 
+        //The stomp interface presented to the rest of the angular app
         function makeWrappedClient(stompClient) {
             var that = {};
             var listenerIds = [];
-            var makeListenerId = function() {
-                var listenerId = Guid();
+            function makeListenerId() {
+                var listenerId = Guid.generate();
                 listenerIds.push(listenerId);
                 return listenerId;
             };
-            that.makeSender = function(destination) {
+            that.makeSender = function(sendDestination) {
                 return function(data) {
-                    stompClient.send(destination, {'content-type':'application/json'}, JSON.stringify(data));
+                    stompClient.send(sendDestination, {'content-type':'application/json'}, JSON.stringify(data));
                 };
             };
-            that.makeTempSender = function(destination, callback) {
+            that.makeTempSender = function(sendDestination, callback) {
                 var listenerId = makeListenerId();
                 var temporaryQueue = '/temp-queue/'+listenerId;
-                _self.registerListener(listenerId, destination, callback);
-                return function(data) {
-                    stompClient.send( id, {
+                _self.registerListener(listenerId, temporaryQueue, callback);
+                return function(msgBody) {
+                    stompClient.send( sendDestination, {
                         'reply-to': temporaryQueue,
                         'content-type':'application/json'
-                    }, JSON.stringify(data));
+                    }, JSON.stringify(msgBody));
                 };
             };
-            that.listenTo = function(destination, callback) {
+            that.send = function(sendDestination, msgBody) {
+                stompClient.send(sendDestination, {'content-type':'application/json'}, JSON.stringify(msgBody));
+            };
+            that.listenTo = function(receiveDestination, callback) {
                 var listenerId = makeListenerId();
-                _self.registerListener(listenerId, destination, callback);
+                _self.registerListener(listenerId, receiveDestination, callback);
+                _self.stompListenTo(receiveDestination);
             };
             that.destroy = function() {
                 angular.forEach(listenerIds, function(listenerId) {
@@ -145,7 +116,8 @@ angular.module('famousAngularStarter')
             return that;
         };
 
-        var listeners = [];
+        //Listeners are the local callbacks, they are all associated with a destination
+        var listeners = {};
         _self.registerListener = function(id, destination, func) {
             listeners[id] = {
                 destination : destination,
@@ -156,100 +128,34 @@ angular.module('famousAngularStarter')
             delete listeners[id];
         };
 
-        var notifyListeners = function (data) {
+        //Here is where the local routing happens.
+        function notifyListeners(msg) {
             angular.forEach(listeners, function(listener) {
-                if(listener.destination === data.headers.destination) {
-                    listener.listenFunction(JSON.parse(data.data));
+                if(listener.destination === msg.headers.destination) {
+                    msg.body = JSON.parse(msg.body);
+                    listener.listenFunction(msg);
                 }
+            });
+        };
+        _self.stompListenTo = function(destination) {
+            stompClient.subscribe(destination, function(d) {
+                notifyListeners(d);
             });
         };
 
         var on_connect = function(x) {
-            stompClient.subscribe(subscribeTo, function(d) {
-                console.log("Message received on permanent channel: "+JSON.stringify(d));
-                notifyListeners(d);
-            });
             deferred.resolve(makeWrappedClient(stompClient));
         };
         var on_error =  function() {
-            console.log('error');
-            throw "Failed to connect to message broker";
+            deferred.reject("Failed to connect to message broker");
         };
+
+        //Called when messages are received on a temporary channel
         stompClient.onreceive = function(m) {
-            console.log("Message on temporary channel: "+JSON.stringify(m));
             notifyListeners(m);
-        }
+        };
+
         stompClient.connect('guest', 'guest', on_connect, on_error, '/');
 
         return deferred.promise;
-
-        return _self;
-    }])
-    .service('AuthenticationService',['$q','StompClientFactory', function($q, StopClientFactory){
-        var self = {};
-
-
     }]);
-    /*
-    .service('StompService', ['$http','IdProvider', function ($http, IdProvider) {
-        var self = {};
-
-        // Stomp.js boilerplate
-        var ws = new SockJS('http://' + window.location.hostname + ':15674/stomp');
-        var client = Stomp.over(ws);
-        // SockJS does not support heart-beat: disable heart-beats
-        client.heartbeat.outgoing = 0;
-        client.heartbeat.incoming = 0;
-
-        var deferred = $q.defer();
-
-        var wrappedClient = (function() {
-            var that = {};
-            that.send = function(data) {
-                client.send('/queue/server', {"content-type":"application/json"}, JSON.stringify(data));
-            };
-            that.sendQ = function(id, data) {
-                client.send('/queue/'+id, {"content-type":"application/json"}, JSON.stringify(data));
-            };
-            that.sendT = function(id, data) {
-                client.send('/topic/'+id, {"content-type":"application/json"}, JSON.stringify(data));
-            };
-            return that;
-        })();
-
-        var listeners = [];
-        self.registerListener = function(id, func) {
-            listeners[id] = func;
-        };
-        self.removeListener = function(id) {
-
-        }
-
-        var notifyListeners = function (data) {
-            angular.forEach(listeners, function(listener) {
-                listener(data);
-            });
-        };
-
-        IdProvider.getId().then(function success(data){
-            var on_connect = function(x) {
-                client.subscribe("/topic/"+data.id, function(d) {
-                    console.log(d.body);
-                    notifyListeners(JSON.parse(d.body));
-                });
-                deferred.resolve(wrappedClient);
-                //client.send('/queue/test', {"content-type":"text/plain"}, 'one two three');
-            };
-            var on_error =  function() {
-                console.log('error');
-                throw "Failed to connect to message broker";
-            };
-            client.connect('guest', 'guest', on_connect, on_error, '/');
-        }, function error(reason){
-
-        });
-        self.getClient = function(){
-            return deferred.promise;
-        };
-        return self;
-    }]);*/
